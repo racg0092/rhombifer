5df0f5913ed6ee54868f15986dbb6039afdf54c5
Richard Chapman Gomez <chapman.gomez.richard@gmail.com>
2024-06-08 01:36:19 -0400

    Added part of the logic top handle short format and long format commands
    Able to identify unrecognizerd commands and stop the operations

--- pkg/parsing/parsing.go
+++ pkg/parsing/parsing.go
@@ -6,26 +6,32 @@ import (
 	"strings"
 )
 
-// Looks up flags in the args provided if any is found is bundle in a slice and return.
+// Looks up flags in the args provided if any is found a map of runable flags (flr) is returned.
 // The function will stop looking for flags if it reaches the end of slice of args or runs into
 // a sub command
-func FlagsLookup(flags map[string]models.Flag, args ...string) (map[string]models.Flag, error) {
+func FlagsLookup(flags map[string]models.Flag, args ...string) (flr map[string]models.Flag, err error) {
 	if flags == nil || len(flags) <= 0 {
-		return flags, fmt.Errorf("Flags is either nil or empty")
+		return nil, fmt.Errorf("Flags is either nil or empty")
 	}
-	foundFlags := make(map[string]models.Flag)
 	for _, a := range args {
-		if strings.HasPrefix(a, "-") {
+		if strings.HasPrefix(a, "-") && !strings.HasPrefix(a, "--") {
 			a = a[1:]
 			for _, shorthand := range a {
 				s := string(shorthand)
-				fmt.Println(s)
+				_, found := flags[s]
+				if !found {
+					return nil, fmt.Errorf("Unrecognized flag %s", s)
+				}
 			}
 		} else if strings.HasPrefix(a, "--") {
-
+			a = a[2:]
+			_, found := flags[a]
+			if !found {
+				return nil, fmt.Errorf("Unrecognized flag %s", a)
+			}
 		} else {
 			break
 		}
 	}
-	return foundFlags, nil
+	return flr, nil
 }
--- pkg/parsing/tests/parsing_test.go
+++ pkg/parsing/tests/parsing_test.go
@@ -8,13 +8,30 @@ import (
 )
 
 func TestFlagsLookup(t *testing.T) {
-	input := "-rfs"
-	flags := make(map[string]models.Flag)
-  flags["r"] = models.Flag{
-    Name: "recursive"
-  }
-	_, err := parsing.FlagsLookup(flags, input)
-	if err != nil {
-		t.Error(err)
-	}
+
+	t.Run("testing short format", func(t *testing.T) {
+		input := "-r"
+		flags := make(map[string]models.Flag)
+		flags["r"] = models.Flag{
+			Name:        "recursive",
+			ShortFormat: "r",
+		}
+		_, err := parsing.FlagsLookup(flags, input)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("testing long format", func(t *testing.T) {
+		input := "--recursive"
+		flags := make(map[string]models.Flag)
+		flags["recursive"] = models.Flag{
+			Name:        "recursive",
+			ShortFormat: "r",
+		}
+		_, err := parsing.FlagsLookup(flags, input)
+		if err != nil {
+			t.Error(err)
+		}
+	})
 }
