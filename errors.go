d621c4347a75261a408861fc0ab1345d786d51fe
Richard Chapman Gomez <chapman.gomez.richard@gmail.com>
2024-07-27 15:26:12 -0400

    Added utility function

--- errors.go
+++ errors.go
@@ -3,5 +3,8 @@ package rhombifer
 import "github.com/racg0092/rhombifer/pkg/errs"
 
 var (
-	ErroNoRootRunFunc = errs.NewError("Root has no Run function. Therefore it can't interpret any flags")
+	ErroNoRootRunFunc = errs.NewError("root has no Run function. Therefore it can't interpret any flags")
+
+	ErroFlagUndefined   = errs.NewError("flag is undefined.")
+	ErroFlagHasNoValues = errs.NewError("flag has no values.")
 )
--- utils.go
+++ utils.go
@@ -1,6 +1,11 @@
 package rhombifer
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/racg0092/rhombifer/pkg/models"
+)
 
 //todo: If custom flag definition is implemented this function will need to be change
 
@@ -10,4 +15,27 @@ func IsFirstArgFlag(arg string) bool {
 		return true
 	}
 	return false
+}
+
+// Extract expecified quantity of values from flag
+func ExtractFlagValues(flag *models.Flag, quantity int) ([]string, error) {
+	vals := make([]string, 0)
+	if flag == nil {
+		return vals, ErroFlagUndefined
+	}
+
+	if len(flag.Values) == 0 {
+		ErroFlagHasNoValues.AppendMessage("flag name: " + flag.Name)
+		return vals, ErroFlagHasNoValues
+	}
+
+	if quantity == 0 {
+		return vals, fmt.Errorf("ExtractFlagValues func must have a quatity parameter equal or greater than 1")
+	}
+
+	for count := 0; count < quantity; count++ {
+		vals = append(vals, flag.Values[count])
+	}
+
+	return vals, nil
 }
